761ae5fbfe9a21e845c4e0691a71d508f5acabdd
Justin Hager <developerjustinhager@gmail.com>
2021-02-06 13:45:24 -0800

    remove log

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,7 @@ export async function getServerSideProps() {
 }
 
 export default function Home({data}) {
-  console.log('data', data);
-  
+    
   return (
     <div className={styles.container}>
       <Head>
